b558b67e1a67ac6257aec4fb619a9697626a88c1
Rajath C R <Rajath.CR@sonata-software.com>
2022-10-28 14:20:52 +0530

    updated

--- variable.tf
+++ variable.tf
@@ -20,7 +20,7 @@ variable "tag_name" {
 
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-098dc95f847244788"
+  default     = "ami-0eb3de4dc4fae08c8"
 }
 
 variable "availability_zone" {
